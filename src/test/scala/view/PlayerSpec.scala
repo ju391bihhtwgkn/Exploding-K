370303d6d11760e86f63d1b917b1a364d0b334f8
WiebkePrinz <94743640+WiebkePrinz@users.noreply.github.com>
2022-10-28 12:42:52 +0200

    Add Player

---
+++ src/test/scala/view/PlayerSpec.scala
@@ -0,0 +1,25 @@
+import model.Player
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class PlayerSpec() extends AnyWordSpec with Matchers {
+  "A Player" when {
+    "set to Julian" should {
+      val player = Player("Julian")
+      "have a name" in {
+        player.name should be("Julian")
+      }
+      "have a nice String representation" in {
+        player.toString should be("Julian")
+      }
+    }
+  }
+  "A Player" when {
+    "at the start of the game" should {
+      val player = Player("Julian")
+      "have not lost" in {
+        player.hasLost should be(false)
+      }
+    }
+  }
+}
