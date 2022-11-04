adc2c9a2acb7fbf90af43571298222b0d3e58538
WiebkePrinz <94743640+WiebkePrinz@users.noreply.github.com>
2022-11-04 11:35:20 +0100

    made main playable

--- src/main/scala/view/main.scala
+++ src/main/scala/view/main.scala
@@ -1,17 +1,24 @@
 package view
-
+import scala.util.control.Breaks._
 import scala.io.StdIn.readLine
 
 object main {
   def main(Args: Array[String]): Unit = {
     var cdeck = Carddeck().addCard(Card("Exploding", "You die"), 2)
     cdeck = cdeck.addCard(Card("Random Card", "Do what you want"), 8)
-    print("Du bist am zug. Möchtest du 1: eine Karte ziehen oder 2: erst die oberen drei Karten anschauen")
-    val choice = readLine()
-    choice match {
-      case "1" => print("Your new Card: " + cdeck.takeCard())
-      case "2" => print("Thats the first three Cards" + cdeck.spy())
+    print("Du bist am zug. Möchtest du 1: eine Karte ziehen oder 2: erst die oberen drei Karten anschauen, keine lust mehr geb str. D ein.\n")
+    while (true){
+      val choice = readLine()
+      choice match {
+        case "1" => {print("Your new Card:\n" + cdeck.takeCard())
+                    cdeck = cdeck.reduce()}
+        case "2" => print("Thats the first three Cards\n" + cdeck.spy())
+        case _ => {print("Spiel beendet\n")
+                    break
+        }
+      }
     }
+
   }
 
 }
