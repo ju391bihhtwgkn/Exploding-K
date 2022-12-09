a41c8ed5709ed15d95f35d8573382d71435934a6
WiebkePrinz <94743640+WiebkePrinz@users.noreply.github.com>
2022-12-09 09:16:54 +0100

    finished toInt()

--- src/main/scala/de/htwg/se/explodingKitten/model/PlayCard.scala
+++ src/main/scala/de/htwg/se/explodingKitten/model/PlayCard.scala
@@ -3,14 +3,27 @@ package de.htwg.se.explodingKitten.model
 import de.htwg.se.explodingKitten.controller.{Controller, GameContext}
 
 import scala.io.StdIn.readLine
+import scala.util.{Failure, Success, Try}
+import scala.util.control.Breaks.{break, breakable}
 
 class PlayCard extends Move {
   override def makeMove(person: Player, carddeck: Controller): Player = {
-    print("Welche Karte wollen Sie spielen? Geben Sie die PlatzNr. der Karte ein:") //Tui aktivieren
-    val cNr = readLine().toInt
+    var inputB = true
+    var cNr = 0
+    while(inputB) {
+      print("Welche Karte wollen Sie spielen? Geben Sie die PlatzNr. der Karte ein:") //Tui aktivieren
+      val s = readLine()
+      toInt(s) match {
+        case None => inputB = true
+        case _ => {
+          inputB = false
+          cNr = toInt(s).get
+        }
+      }
+    }
+
     val card = person.chooseCardToPlay(cNr)
     var newPerson = person.playCard(card)
-    //print(newPerson.handCards)
     card.actionCode match {
       case 2 => {
         carddeck.spy()
@@ -27,5 +40,12 @@ class PlayCard extends Move {
         newPerson
       }
     }
+  }
+
+  def toInt(s : String) : Option[Int] = {
+      Try(s.toInt) match{
+        case Success(value) => return Some(s.toInt)
+        case Failure(exception) => return None
+      }
   }
 }
