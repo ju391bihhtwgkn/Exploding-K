1351741abe025b5a01979f9d1ec202dc4d1e3914
ju391bihhtwgkn <bihl.julian@web.de>
2023-01-20 12:03:52 +0100

    removed null

--- src/main/scala/de/htwg/se/explodingKitten/ExplodingKittenModule.scala
+++ src/main/scala/de/htwg/se/explodingKitten/ExplodingKittenModule.scala
@@ -5,13 +5,14 @@ import de.htwg.se.explodingKitten.controller.ContextComponent._
 import de.htwg.se.explodingKitten.controller.ContextComponent.contextBaseimplementation.GameContext
 import de.htwg.se.explodingKitten.controller.ControllerComponent.ControllerInterface
 import de.htwg.se.explodingKitten.controller.ControllerComponent.controllerBaseImplementation.Controller
-import de.htwg.se.explodingKitten.model.GameStateComponent.{Card}
+import de.htwg.se.explodingKitten.model.GameStateComponent.Card
 import de.htwg.se.explodingKitten.model.GameStateComponent.GameStateBaseimplementation.Gamestate
 import de.htwg.se.explodingKitten.model.PlayerComponent.Player
+import de.htwg.se.explodingKitten.model.StrategyComponent.TakeCard
 
 class ExplodingKittenModule extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[ControllerInterface]).toInstance(Controller(Gamestate(0, Vector[Player](), Vector[Card](), Vector(Card("FeralCat")))))
-    bind(classOf[ContextInterface]).toInstance(GameContext(null))
+    bind(classOf[ContextInterface]).toInstance(GameContext(new TakeCard))
   }
 }
