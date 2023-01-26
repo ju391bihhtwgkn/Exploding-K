a8453c48f1cd96b7eb95c8c5cd850bd9519b7fe9
WiebkePrinz <94743640+WiebkePrinz@users.noreply.github.com>
2023-01-26 20:39:50 +0100

    test fix 1.1

--- src/test/scala/de/htwg/se/explodingKitten/model/StateSpec.scala
+++ src/test/scala/de/htwg/se/explodingKitten/model/StateSpec.scala
@@ -11,8 +11,48 @@ class StateSpec extends AnyWordSpec with Matchers{
   CardDeck.initializeDeck()
   val disPile = Vector[Card]
 
-  "Make a Gamestate" in {
-    GameStateInterface()
+  "Check Status" when{
+    "Status Play beginn" in{
+      player.state shouldBe a [waitingStatus]
+    }
+    "Set Status" in {
+      player.changeState(new playingStatus(player))
+      player.state shouldBe a [playingStatus]
+    }
+  }
+
+  "Check out what the methods do" when{
+    "State should have Changed from Playing to Waiting" in {
+      player.changeState(new playingStatus(player))
+      player.state.onPlay()
+      player.state shouldBe a [waitingStatus]
+    }
+    "State should have Changed form Playing to Waiting" in {
+      player.changeState(new playingStatus(player))
+      player.state.onLock()
+      player.state shouldBe a [waitingStatus]
+    }
+    "State should have Changed from Waiting to  Playing" in {
+      player.changeState(new playingStatus(player))
+      player.state.onLock()
+      player.state.onPlay()
+      player.state shouldBe a [playingStatus]
+    }
+    "State should have Changed nothing" in {
+      player.changeState(new playingStatus(player))
+      player.state.onLock()
+      player.state.onLock()
+      player.state shouldBe a [waitingStatus]
+    }
+    "State should have Changed nothing in Death" in {
+      player.changeState(new playingStatus(player))
+      player.setHasLost()
+      player.state.onPlay()
+      player.state.onLock()
+      player.state shouldBe a [diedStatus]
+      player.state.onPlay()
+      player.state shouldBe a [diedStatus]
+    }
   }
 }
 
